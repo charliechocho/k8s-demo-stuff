de5f4c993bb56385b3b123a72de1543ce40d4e0c
charliechocho <msoderberg@vmware.com>
2023-03-27 19:57:45 +0200

    Deleted some unused files

--- dockerSecret.sh
+++
@@ -1,1 +0,0 @@
-kubectl create secret -n $1 docker-registry dockersecret --docker-server=registry.tanzu.dk --docker-username=msoderberg --docker-password=<passwd> --docker-email=msoderberg@vmware.com 
