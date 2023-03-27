8f6ddf88d4931c872cd492cf9e42f92173ccfac0
charliechocho <msoderberg@vmware.com>
2023-03-27 18:18:15 +0200

    changed values in clu.yaml

--- makeFiles/tkc-chngname-dev.py
+++ makeFiles/tkc-chngname-dev.py
@@ -2,6 +2,7 @@ import yaml
 
 filename = 'clu.yaml'
 clu_name = input('What name do you want for your cluster?: ')
+ctrl_node = int(input('How many Controlplanes? ') or 1)
 
 if not clu_name:
     print('You have to enter a valid cluster name!! Try again')
@@ -13,11 +14,12 @@ def load_yaml():
         data = yaml.load(file, Loader=yaml.FullLoader)
         return data
          
-def chng_yaml(yml, clu_name):
+def chng_yaml(yml, clu_name, ctrl_node):
     """Takes input through name and changes value in yml file"""
     with open(filename, 'w') as file:
         yml['metadata']['name'] = clu_name
+        yml['spec']['topology']['controlPlane']['replicas'] = ctrl_node
         yaml.dump(yml, file)
 
 yml = load_yaml()
-chng_yaml(yml, clu_name)
+chng_yaml(yml, clu_name, ctrl_node)
