a1d825c92dd66dcd5d82e82c807c2da73f1e7917
charliechocho <msoderberg@vmware.com>
2023-03-27 18:37:48 +0200

    Updated tkc-chngname.py with new version

--- makeFiles/tkc-chngname.py
+++ makeFiles/tkc-chngname.py
@@ -2,6 +2,12 @@ import yaml
 
 filename = '/Users/msoderberg/h2o/k8s-demo-stuff/clusterYamls/clu.yaml'
 clu_name = input('What name do you want for your cluster?: ')
+ctrl_node = int(input('How many Controlplanes? ') or 1)
+wrk_node = int(input('How many Workernodes? ') or 1)
+
+if not clu_name:
+    print('You have to enter a valid cluster name!! Try again')
+    exit()
 
 def load_yaml():
     """Load TGC yaml file"""
@@ -9,11 +15,13 @@ def load_yaml():
         data = yaml.load(file, Loader=yaml.FullLoader)
         return data
          
-def chng_yaml(yml, clu_name):
+def chng_yaml(yml, clu_name, ctrl_node, wrk_node):
     """Takes input through name and changes value in yml file"""
     with open(filename, 'w') as file:
         yml['metadata']['name'] = clu_name
+        yml['spec']['topology']['controlPlane']['replicas'] = ctrl_node
+        yml['spec']['topology']['nodePools'][0]['replicas'] = wrk_node
         yaml.dump(yml, file)
 
 yml = load_yaml()
-chng_yaml(yml, clu_name)
+chng_yaml(yml, clu_name, ctrl_node, wrk_node)
