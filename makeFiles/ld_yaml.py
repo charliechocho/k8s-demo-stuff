8b0bb6ac7491a2233c881a0ad76434682a50fe2b
charliechocho <msoderberg@vmware.com>
2023-03-28 19:55:43 +0200

    updated code and ready for full test

--- makeFiles/ld_yaml.py
+++ makeFiles/ld_yaml.py
@@ -1,16 +1,18 @@
 import yaml
-filename = '/Users/msoderberg/h2o/k8s-demo-stuff/clusterYamls/clu.yaml'
-
+#filename = '/Users/msoderberg/h2o/k8s-demo-stuff/clusterYamls/clu.yaml'
+filename = 'clu.yaml'
 def load_yaml(filename):
     """Load TGC yaml file"""
     with open(filename, 'r') as file:
         data = yaml.load(file, Loader=yaml.FullLoader)
         return data
          
-def chng_yaml(yml, clu_name, ctrl_node, wrk_node):
+def chng_yaml(yml, clu_name, ctrl_node, wrk_node, cp_vm, wn_vm):
     """Takes input through name and changes value in yml file"""
     with open(filename, 'w') as file:
         yml['metadata']['name'] = clu_name
         yml['spec']['topology']['controlPlane']['replicas'] = ctrl_node
+        yml['spec']['topology']['controlPlane']['vmClass'] = cp_vm
         yml['spec']['topology']['nodePools'][0]['replicas'] = wrk_node
+        yml['spec']['topology']['nodePools'][0]['vmClass'] = wn_vm
         yaml.dump(yml, file)
--- makeFiles/tkc-chngname.py
+++ makeFiles/tkc-chngname.py
@@ -16,13 +16,15 @@ else:
     ctrl_node = int(input('How many Controlplanes? (press ENTER for 1) ') or 1)
     wrk_node = int(input('How many Workernodes? (press ENTER for 1) ') or 1)
 
-#load cluster yaml file and change name and replica values
-yml = load_yaml(yamlfile)
-chng_yaml(yml, clu_name, ctrl_node, wrk_node)
-
 #find out what machine class available and present choices to chose from. Then change in clu.yaml
 mcfile = build_machine_class()
 choices = vmcl_parser.mc_pull(mcfile, verlist)
 vmcl_parser.prn_choice(choices)
 ctrl, work = vmcl_parser.get_mc()
 ctrl_vm, work_vm = vmcl_parser.mc_choice(choices, ctrl, work)
+
+
+#load cluster yaml file and change name and replica values
+yml = load_yaml(yamlfile)
+chng_yaml(yml, clu_name, ctrl_node, wrk_node, ctrl_vm, work_vm)
+print('\n\nYour Create TKC Cluster yaml is updated and ready to go!')
