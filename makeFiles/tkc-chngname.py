ed6e6473323dbf9a506ac68e0ed00e987125c6b1
charliechocho <msoderberg@vmware.com>
2023-03-28 18:48:53 +0200

    added more code

--- makeFiles/tkc-chngname.py
+++ makeFiles/tkc-chngname.py
@@ -1,7 +1,11 @@
 from ld_yaml import load_yaml, chng_yaml
+from mc import build_machine_class
+import vmcl_parser
 
 #put cluter yaml in variable and ask for cluster name
-filename = '/Users/msoderberg/h2o/k8s-demo-stuff/clusterYamls/clu.yaml'
+yamlfile = '/Users/msoderberg/h2o/k8s-demo-stuff/clusterYamls/clu.yaml'
+verlist = {}
+
 clu_name = input('What name do you want for your cluster?: ')
 
 #check if a valid cluster name given, if so ask for number of controllers and workers
@@ -13,5 +17,12 @@ else:
     wrk_node = int(input('How many Workernodes? (press ENTER for 1) ') or 1)
 
 #load cluster yaml file and change name and replica values
-yml = load_yaml(filename)
+yml = load_yaml(yamlfile)
 chng_yaml(yml, clu_name, ctrl_node, wrk_node)
+
+#find out what machine class available and present choices to chose from. Then change in clu.yaml
+mcfile = build_machine_class()
+choices = vmcl_parser.mc_pull(mcfile, verlist)
+vmcl_parser.prn_choice(choices)
+ctrl, work = vmcl_parser.get_mc()
+ctrl_vm, work_vm = vmcl_parser.mc_choice(verlist, ctrl, work)
---
+++ makeFiles/vmcl_parser.py
@@ -0,0 +1,30 @@
+
+def mc_pull(filename, verlist):
+    """Reads the vmclass.txt file and builds a dict of choices"""
+    with open(filename, 'r') as tf:
+        choice = 1
+        for line in tf:
+            data = line.split()
+            if 'NAME' not in data:
+                verlist[choice] = data[0]
+                choice += 1
+        
+        return verlist
+
+def prn_choice(verlist):
+    """Iterates through the ver_list dict and prints a choice list"""
+    for key, value in verlist.items():
+        print(f"{key}.\t{value}")
+
+def get_mc():
+    ctrl = input('\nEnter the number of the desired control node VM Class: ')
+    work = input('\nEnter the number of the desired worker node VM Class: ')
+    return ctrl, work
+
+def mc_choice(verlist, ctrl, work):
+    try: 
+        k8s_ctrl = verlist[int(ctrl)]
+        k8s_work = verlist[int(work)]
+        return k8s_ctrl, k8s_work
+    except KeyError:
+        print(f'One of the VM Classes is not available')
