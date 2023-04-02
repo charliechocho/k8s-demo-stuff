96a8e23512ab761fecd231f5fe8aa6f37ebe03e5
charliechocho <msoderberg@vmware.com>
2023-04-02 17:40:16 +0200

    copied files

---
+++ demoApps/rmq_receive.py
@@ -0,0 +1,33 @@
+#!/usr/local/bin/ python3
+
+import pika, sys, os
+
+credentials = pika.PlainCredentials('msoderberg', 'VMware1!')
+parameters = pika.ConnectionParameters('10.220.3.41',
+                                   5672,
+                                   '/',
+                                   credentials)
+
+def main():
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+
+    channel.queue_declare(queue='Tanzu Cities')
+
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
+
+    channel.basic_consume(queue='Tanzu Cities', auto_ack=True, on_message_callback=callback)
+
+    print(' [*] Waiting for messages! To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
---
+++ demoApps/rmq_send.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/ python3
+
+import pika
+from faker import Faker
+names = []
+
+fake = Faker()
+
+credentials = pika.PlainCredentials('msoderberg', 'VMware1!')
+parameters = pika.ConnectionParameters('10.220.3.41',
+                                   5672,
+                                   '/',
+                                   credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='Tanzu Cities')
+
+for i in range(10):
+	names.append(fake.city())
+
+for fName in names:
+	channel.basic_publish(exchange='',
+                  routing_key='Tanzu Cities',
+                  body=f'{fName}')
+	print(f" [x] Sent {fName}!")
+connection.close()
