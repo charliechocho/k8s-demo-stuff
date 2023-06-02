25935c06db3087765a3281850e45a699b1907daa
charliechocho <msoderberg@vmware.com>
2023-06-03 00:32:07 +0200

    dummy py file

---
+++ rabbitmq/contact_sig.py
@@ -0,0 +1,7 @@
+def contact_info(Tanzu_SE):
+    name = 'Mattias Söderberg'
+    phone = '+46709524869'
+    email = 'msoderberg@vmware.com'
+    title = 'Lead Solution Engineer'
+
+    return 'https://tanzu.academy'
