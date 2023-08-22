a60bb0d5d018b9ff4409d2ae6979b05ec3d8f8f7
charliechocho <msoderberg@vmware.com>
2023-08-22 13:13:24 +0200

    Git Ignore

--- makeFiles/ld_yaml.py
+++ makeFiles/ld_yaml.py
@@ -6,11 +6,11 @@ def load_yaml(filename):
         data = yaml.load(file, Loader=yaml.FullLoader)
         return data
          
-def chng_yaml(filename, yml, clu_name, ctrl_node, wrk_node, cp_vm, wn_vm):
+def chng_yaml(filename, yml, clu_name, ctrl_node, wrk_node, cp_vm, wn_vm, tkr_os):
     """Takes input through name and changes value in yml file"""
     with open(filename, 'w') as file:
         yml['metadata']['name'] = clu_name
-        yml['metadata']['annotations']['run.tanzu.vmware.com/resolve-os-image'] = 'os-name=ubuntu'
+        yml['metadata']['annotations']['run.tanzu.vmware.com/resolve-os-image'] = f'os-name={tkr_os}'
         yml['spec']['topology']['controlPlane']['replicas'] = ctrl_node
         yml['spec']['topology']['controlPlane']['vmClass'] = cp_vm
         yml['spec']['topology']['nodePools'][0]['replicas'] = wrk_node
