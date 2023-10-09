4d630b3a2759b3ff622564f09d11132521f8f93e
charliechocho <msoderberg@vmware.com>
2023-10-09 10:50:39 +0200

    changed pg-instance yaml and pgoperator script

--- postgres/03_installPgOperator.sh
+++ postgres/03_installPgOperator.sh
@@ -1,1 +1,1 @@
-helm install pg-operator postgres-for-kubernetes-v2.2.0/operator --namespace=pg-db --wait    
+helm install pg-operator postgres-for-kubernetes-v2.1.0/operator --namespace=pg-db --wait    
