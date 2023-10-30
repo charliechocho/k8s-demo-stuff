36a6a48ea3c430202ef49ddff97695cd6b6b6038
charliechocho <msoderberg@vmware.com>
2023-10-30 15:08:04 +0000

    removed files from TBS Repo

--- builservice-install/01_tbs-env.sh
+++
@@ -1,5 +0,0 @@
-export INSTALL_REGISTRY_USERNAME=msoderberg
-export INSTALL_REGISTRY_PASSWORD=Th3darkhalf!
-export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.dk
-export TAP_VERSION=1.6.4
-export INSTALL_REPO=tanzu-build-service
--- builservice-install/02_tanzu-reg-cred.sh
+++
@@ -1,5 +0,0 @@
-tanzu secret registry add tanzunet-registry \
-  --username "msoderberg" \
-  --password "Th3Darkhalf!" \
-  --server "registry.tanzu.vmware.com" \
-  --yes --namespace tap-install
