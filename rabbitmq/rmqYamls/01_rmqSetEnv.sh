739cc4c0d30b74c29d1e00abd5e94f0ae649da0a
charliechocho <msoderberg@vmware.com>
2023-10-30 16:30:45 +0000

    removed file for security

--- rabbitmq/rmqYamls/01_rmqSetEnv.sh
+++
@@ -1,9 +0,0 @@
-# Sets up env för Kapp och SecretGen Controller installation - startas med <. ./rmqSetEnv.sh>
-# Then you start the installation
-# </> cd $HOME/tanzu-cluster-essentials </>
-# </> ./install.sh --yes </>
-
-export INSTALL_BUNDLE=registry.tanzu.vmware.com/tanzu-cluster-essentials/cluster-essentials-bundle@sha256:2354688e46d4bb4060f74fca069513c9b42ffa17a0a6d5b0dbb81ed52242ea44
-export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
-export INSTALL_REGISTRY_USERNAME=msoderberg@vmware.com
-export INSTALL_REGISTRY_PASSWORD=Th3darkhalf!
--- rabbitmq/rmqYamls/03_rabbitMQSecret.sh
+++
@@ -1,1 +0,0 @@
-kubectl create secret -n $1 docker-registry tanzu-rabbitmq-registry-creds --docker-server=https://registry.tanzu.vmware.com/ --docker-username='msoderberg@vmware.com' --docker-password='Th3darkhalf!'
