8e0f3dd17f97e26aa2f1ad5d89e2fba76e49df5c
charliechocho <msoderberg@vmware.com>
2024-01-10 10:25:11 +0100

    Still trying to get the UUID right

--- pythonStuff/faApp/app.py
+++ pythonStuff/faApp/app.py
@@ -1,9 +1,17 @@
 import requests
 
-getres = requests.get('http://127.0.0.1:8000/api/v1/users')
-
-res = getres.json()
-
-for item in res:
-    print(item)
+def list_users():
+    getres = requests.get('http://127.0.0.1:8000/api/v1/users')
+    res = getres.json()
+    for item in res:
+        print(item.get('id'))
     
+def update_user(uid, update):
+    useput = requests.put(f'http://127.0.0.1:8000/api/v1/users/{uid}', data=update)
+
+list_users()
+update = '{"first_name":"Doug", "last_name":"Dog", "roles":["user"]}'
+uid = input('Enter User ID you want to update: ')
+update_user(uid, update)
+
+ 
