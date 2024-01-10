58cdfe194094fc9647a2a9a4b0f494410c1011ae
charliechocho <msoderberg@vmware.com>
2024-01-10 19:32:57 +0100

    fixed string format for POST request

--- pythonStuff/faApp/create-user.py
+++ pythonStuff/faApp/create-user.py
@@ -1,31 +1,37 @@
 from uuid import uuid4
 import requests
+import json
 
 def create_uuid():
     id = uuid4()
-    return str(id)
+    return id
 
-def create_user():
+def create_user(id):
     userdict = {}
+    userdict["id"] = str(id)
     userdict["first_name"] = input('Enter user first name: ')
     userdict["last_name"] = input('Enter user last name: ')
     userdict["gender"] = input('Enter user gender: ')
     roles = input('Enter role of the user (press enter for "user"): ') or "user"
     userdict["roles"] = [roles]
     
-    return str(userdict)
+    return json.dumps(userdict)
     
 def post_user(user):    
-    mkeusr = requests.post('http://127.0.0.1:8000/api/v1/users', json=user)
+    mkeusr = requests.post('http://127.0.0.1:8000/api/v1/users', data=user)
 
 id = create_uuid()
-user = create_user()
-answ = input('Do you want to create this user(Y/N)? ("Press Enter for Y ")') or 'y'
+user = create_user(id)
+answ = input('Do you want to create this user(Y/N)? ("Press Enter for Y ") ') or 'y'
 
-# print(f"'{user}'")
+print(user)
+# print(id)
+# print(type(id))
+# print(user["id"])
+
 if answ.lower() == 'y':
     try:
-        post_user(f"'{user}'")
+        post_user(user)
     except requests.exceptions.ConnectionError as error:
         print(f"Errormessage : {error}\nCan't reach API endpoint!")        
 else:
