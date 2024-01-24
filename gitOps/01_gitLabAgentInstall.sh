41628f93f7b05e8c2669ea9b04a0235d6f234f84
charliechocho <msoderberg@vmware.com>
2024-01-24 18:32:07 +0100

    edited the nginxIngress yaml

--- gitOps/01_gitLabAgentInstall.sh
+++
@@ -1,8 +0,0 @@
-helm repo add gitlab https://charts.gitlab.io
-helm repo update
-helm upgrade --install testk8s gitlab/gitlab-agent \
-    --namespace gitlab-agent-testk8s \
-    --create-namespace \
-    --set image.tag=v16.9.0-rc2 \
-    --set config.token=glagent-Vz41t-LekdrGQVPriDBm7CS_Nk1RzrkrMykrDL6SufGBJiCexg \
-    --set config.kasAddress=wss://kas.gitlab.com
